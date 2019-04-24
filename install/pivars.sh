a3bf093c050f4b4c122604b357271957f920ad26
julianes10 <julian.espinosa.santos@ericsson.com>
2019-04-24 22:16:35 +0200

    install por parcesec

--- install/pivars.sh
+++ install/pivars.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
-PROJECT_NAME=carem
+PROJECT_NAME=parcesec
 DEPLOY_FOLDER=/opt/$PROJECT_NAME
-SERVICES_LIST="piInoAgent gaIpaDispatcher kodi"
+SERVICES_LIST="timelapse wvdial telegramBOT wildcardService"
+#SERVICES_LIST="timelapse"
 PI_USER=pi
-PI_IPNAME=pi3
+PI_IPNAME=192.168.1.40
 PI_PORT=22
 
 
