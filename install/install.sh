7b4811289d2ed0ceaa49a2100182286bf0112cc9
julianes10 <julian.espinosa.santos@ericsson.com>
2019-04-24 22:18:28 +0200

    tunning parecec files

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash 
+echo "## STARTING CUSTOM ACTIONS BEFORE INSTALLING"
+source ./install/pivars.sh 
+echo "Umounting timelapse folder..."
+ssh -p $PI_PORT pi@$PI_IPNAME "sudo umount /opt/parcesec/timelapse/static/tmp/timelapse.media"
+echo "## ENDED CUSTOM ACTIONS BEFORE INSTALLING"
+
+
 echo "Here we go: $@"
 echo "------------------------------------------------------------------"
 echo "- This script must me executed from root project                 -"  
