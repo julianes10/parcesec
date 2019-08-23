dfd3eea5a74e66147b7934e176f67fa40e45e3ac
julianes10 <julianes10@yahoo.com>
2019-08-23 02:28:51 +0200

    more actions based on render image from web

--- install/pivars.sh
+++ install/pivars.sh
@@ -1,11 +1,9 @@
 #!/bin/bash 
 PROJECT_NAME=parcesec
 DEPLOY_FOLDER=/opt/$PROJECT_NAME
-SERVICES_LIST="timelapse wvdial telegramBOT wildcardService netMonitor"
+SERVICES_LIST="wvdial telegramBOT netMonitor"
+#SERVICES_LIST="timelapse wvdial telegramBOT wildcardService netMonitor"
 #SERVICES_LIST="timelapse"
 PI_USER=pi
-PI_IPNAME=192.168.1.40
+PI_IPNAME=192.168.1.49
 PI_PORT=22
-
-
-
