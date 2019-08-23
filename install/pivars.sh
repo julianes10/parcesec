76080cce66b2233f2d091ac02ac7cc96f992a49a
julianes10 <julianes10@yahoo.com>
2019-08-23 16:30:26 +0200

    ngrok simple service is also used for remote assistant

--- install/pivars.sh
+++ install/pivars.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 PROJECT_NAME=parcesec
 DEPLOY_FOLDER=/opt/$PROJECT_NAME
-SERVICES_LIST="wvdial telegramBOT netMonitor"
+SERVICES_LIST="wvdial telegramBOT netMonitor ngrok"
 #SERVICES_LIST="timelapse wvdial telegramBOT wildcardService netMonitor"
 #SERVICES_LIST="timelapse"
 PI_USER=pi
-PI_IPNAME=192.168.1.49
+PI_IPNAME=192.168.1.50
 PI_PORT=22
