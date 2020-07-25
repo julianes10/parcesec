feb90b1628a9c7cd50829087f6e10f35ef342b2e
julianes10 <julianes10@yahoo.com>
2020-07-25 12:33:58 +0200

    refactor in deploying scripts

--- install/install.sh
+++ install/install.sh
@@ -1,16 +1,4 @@
 #!/bin/bash 
-echo "## STARTING CUSTOM ACTIONS BEFORE INSTALLING"
-source ./install/pivars.sh 
-echo "Umounting bind folders..."
-ssh -p $PI_PORT pi@$PI_IPNAME "sudo umount /opt/parcesec/timelapse/static/tmp/timelapse.media"
-ssh -p $PI_PORT pi@$PI_IPNAME "sudo umount /opt/parcesec/parcesec/static/tmp/parcesec.media"
-
-echo "Stopping haproxy.."
-ssh -p $PI_PORT pi@$PI_IPNAME "sudo systemctl stop haproxy"
-
-echo "## ENDED CUSTOM ACTIONS BEFORE INSTALLING"
-
-
 echo "Here we go: $@"
 echo "------------------------------------------------------------------"
 echo "- This script must me executed from root project                 -"  
@@ -20,10 +8,6 @@ cmd="./SCS/utils/deploySoftware.sh ./install/pivars.sh $@"
 echo "Executing $cmd"
 $cmd
 aux=$?
-echo "## STARTING CUSTOM ACTIONS AFTER INSTALLING"
-echo "Starting haproxy.."
-ssh -p $PI_PORT pi@$PI_IPNAME "sudo systemctl start haproxy"
-echo "## ENDED CUSTOM ACTIONS AFTER INSTALLING"
 exit $aux
 
 
