44264cbb1d410f1cf5886dfd6be7ead37afc4e6b
julianes10 <julianes10@yahoo.com>
2020-07-31 08:25:23 +0200

    config changes to work all together with basic timelapse capabilities

--- install/custom-install.pre.sh
+++ install/custom-install.pre.sh
@@ -1,6 +1,6 @@
 #### NOTE IT WILL BE EXECUTED BY ROOT IN THE TARGET NOT IN LOCAL PC"
 #### CUSTOM ACTIONS FOR THIS PROJECT BEFORE INSTALLING"
-umount "/opt/parcesec/timelapse/static/tmp/timelapse.media"
+umount "/opt/parcesec/timelapse/static/var/timelapse.media"
 umount "/opt/parcesec/parcesec/static/tmp/parcesec.media"
 systemctl stop haproxy
 
